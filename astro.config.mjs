9aafe69c718a83db5ba4b4d97c8c2a868b04b0e5
ianlp <casualhouse@yahoo.com>
2024-10-07 17:52:00 -0500

    file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 //import preact from "@astrojs/preact";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
-  //integrations: [icon(), preact()],
+  site: "https://www.westtownwebsites.com/",
   integrations: [icon()],
+  output: "static",
+  adapter: vercel(),
 });
